31f344deed6c9d54b02190f9b5483e5892c04b47
Yiwei Zhang <yiwei_zhang@ucsb.edu>
2022-11-07 10:35:41 -0800

    add KillText.cs

---
+++ Assets/KillText.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using UnityEngine.UI;
+using TMPro;
+
+
+// Use following command with inputMessage to show the message for 3 seconds:
+// KillText.instance.ChangeAmount(string inputMeassge);
+public class KillText : MonoBehaviour
+{   public static KillText instance;
+    public TextMeshProUGUI text;
+    public int second = 3;
+    public bool flag = false;
+
+    void Start(){
+        if(instance == null){
+            instance = this;
+        }
+        ChangeAmount("MessageInput");
+    }
+
+     public void ChangeAmount(string inputMessage){
+//        int amount = int.Parse(text.text);
+        text.text =  inputMessage;
+        second = 3;
+        flag = true;
+    }
+
+    public void resetAmount()
+    {
+        text.text = "";
+        flag = false;
+        second = 3;
+    }
+
+    private float nexttime=1;
+    // [Obsolete]
+    public void Update(){
+        if(flag){
+            if(Time.time >= nexttime){
+                second--;
+                nexttime = Time.time + 1;
+                if(second <= 0){
+                    resetAmount();
+                }
+            }
+        }else{
+            text.text = "";
+        }
+        
+    }
+}
